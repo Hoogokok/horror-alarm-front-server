d1239caa16401bf158d971893b9856f38de1e6c2
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-04 09:36:13 +0900

    feat: 프론트앤드에서 fcm을 이용하기 위해 초기화 및 메시징 구독 및 구독 취소 추가.

---
+++ src/main/horror-alram-front/src/messaging.js
@@ -0,0 +1,72 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "horror-alarm.firebaseapp.com",
+  projectId: "horror-alarm",
+  storageBucket: "horror-alarm.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MEASUREMENT_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+
+async function requestPermission() {
+  console.log('Requesting permission...');
+  try {
+    await Notification.requestPermission();
+    console.log('Permission granted');
+  }
+  catch (error) {
+    console.error('Permission denied', error);
+  }
+
+  try {
+    const token = await getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY });
+    const response = await axios.post('/api/token', { token: token });
+    console.log('Token saved:', response.data);
+  }
+  catch (error) {
+    console.error('An error occurred while retrieving token. ', error);
+  }
+}
+
+async function deleteToken() {
+  const swRegistration = messaging.swRegistration;
+  const token = await getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY });
+  console.log('Token before deletion:', token);
+  swRegistration.pushManager.getSubscription().then((subscription) => {
+    subscription.unsubscribe().then((successful) => {
+      console.log('Token deletion successful:', successful);
+      axios.delete('/api/token', { data: { token: token } });
+    }).catch((error) => {
+      console.error('Token deletion failed:', error);
+    });
+  }
+  ).catch((error) => {
+    console.error('Token deletion failed:', error);
+  });
+}
+
+function Subscribe() {
+  return (
+    <button onClick={requestPermission}>Subscribe</button>
+  );
+}
+
+function Unsubscribe() {
+  return (
+    <button onClick={deleteToken}>Unsubscribe</button>
+  );
+}
+
+export { Subscribe, Unsubscribe };
