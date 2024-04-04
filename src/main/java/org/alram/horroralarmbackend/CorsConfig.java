20aed54399bcf6f4e47f66470c986c0b480f58ce
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-04 12:29:15 +0900

    refactor: CorConfig 클래스 디렉토리를 옮김

--- src/main/java/org/alram/horroralarmbackend/CorsConfig.java
+++ src/main/java/org/alram/horroralarmbackend/CorsConfig.java
@@ -1,3 +1,5 @@
+package org.alram.horroralarmbackend;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
