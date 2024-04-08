2ee95ead0c58ac55a40a65f1de36b4ad46dcd7c0
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-08 19:23:02 +0900

    refactor: 클라이언트 토큰을 받는 API에서 토큰을 받아 저장하게 바꿈

--- src/main/java/org/alram/horroralarmbackend/TokenAPIController.java
+++ src/main/java/org/alram/horroralarmbackend/TokenAPIController.java
@@ -1,8 +1,8 @@
 package org.alram.horroralarmbackend;
 
 import lombok.extern.slf4j.Slf4j;
+import org.alram.horroralarmbackend.token.TokenService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class TokenAPIController {
+    private final TokenService tokenService;
+
+    public TokenAPIController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
 
     @PostMapping("/api/token")
-    public ResponseEntity<String> getToken(@RequestBody String token) {
-        log.info("Token: {}", token);
-        return ResponseEntity.ok(token);
+    public ResponseEntity<String> getToken(@RequestBody TokenDTO token) {
+        tokenService.saveToken(token);
+        return ResponseEntity.ok("알람이 설정되었습니다.");
     }
 }
