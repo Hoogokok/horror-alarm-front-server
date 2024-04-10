1bde734657e1a066af53dad78693ada046347787
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-10 10:28:57 +0900

    feat: 구독 취소 시 토큰을 지우는 기능 추가

--- src/main/java/org/alram/horroralarmbackend/token/TokenAPIController.java
+++ src/main/java/org/alram/horroralarmbackend/token/TokenAPIController.java
@@ -1,7 +1,9 @@
 package org.alram.horroralarmbackend.token;
 
 import lombok.extern.slf4j.Slf4j;
+import org.alram.horroralarmbackend.messaging.FirebaseMessagingService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,12 @@ public class TokenAPIController {
     public ResponseEntity<String> getToken(@RequestBody TokenDTO token) {
         tokenService.saveToken(token);
         return ResponseEntity.ok("알람이 설정되었습니다.");
+    }
+
+    @DeleteMapping("/api/unsubscribe")
+    public ResponseEntity<String> deleteToken(@RequestBody TokenDTO token) {
+        FirebaseMessagingService.deleteToken(token.getToken());
+        tokenService.deleteToken(token);
+        return ResponseEntity.ok("알람이 해제되었습니다.");
     }
 }
--- src/main/java/org/alram/horroralarmbackend/token/TokenService.java
+++ src/main/java/org/alram/horroralarmbackend/token/TokenService.java
@@ -1,8 +1,11 @@
 package org.alram.horroralarmbackend.token;
 
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Transactional
 @Service
 public class TokenService {
@@ -15,5 +18,14 @@ public class TokenService {
     public void saveToken(TokenDTO tokenDTO){
         Token token = new Token(tokenDTO.getToken());
         tokenRepository.save(token);
+    }
+
+    public void deleteToken(TokenDTO token) {
+        Optional<Token> byToken = tokenRepository.findByToken(token.getToken());
+        if (!byToken.isPresent()) {
+            log.error("Token not found");
+            return;
+        }
+        tokenRepository.delete(byToken.get());
     }
 }
