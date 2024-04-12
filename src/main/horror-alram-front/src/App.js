f3ebb1a7cc6078f4bea828f58de43c1b6e7723cc
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-12 21:22:00 +0900

    refactor: 알림 설정, 호러 영화 개봉예정일 구독, 구독 취소, 알람 설정 해제 버튼을 따로 만듦

--- src/main/horror-alram-front/src/App.js
+++ src/main/horror-alram-front/src/App.js
@@ -1,15 +1,22 @@
-import { Subscribe, Unsubscribe } from "./messaging";
+import { Subscribe, Unsubscribe, Send, AlarmPermission, DismissingAlarms } from "./messaging";
 
 function App() {
   return (
     <div>
+      <h1>알람 허용하기</h1>
+      <AlarmPermission />
+      <h1>알람 삭제하기</h1>
+      <DismissingAlarms />
+      <p>클릭해서 구독하고 호러 영화 개봉 알람을 받아보세요</p>
       <Subscribe />
-      <h1>Horror Alarm</h1>
-      <p>클릭해서 구독하고 알람을 받아보세요</p>
+      <p>
+        호러 영화 개봉 알람을 받고 싶지 않다면 구독을 취소하세요.
+      </p>
       <Unsubscribe />
       <p>
-        알람을 받지 않으려면 클릭하세요
+        호러 영화 개봉 알람을 테스트하려면 누르세요
       </p>
+      <Send />
     </div>
   );
 }
