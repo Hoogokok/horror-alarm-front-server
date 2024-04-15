e145db942cb373fd7b4cb5470fa29745bfcd38fa
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-15 18:11:05 +0900

    feat: 개봉 예정 영화 API와 프로튼엔드로 보낼 데이터 객체 추가

---
+++ src/main/java/org/alram/horroralarmbackend/upcoming/UpcomingMovieController.java
@@ -0,0 +1,21 @@
+package org.alram.horroralarmbackend.upcoming;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UpcomingMovieController {
+
+    private final UpcomingMovieService upComingMovieService;
+
+    public UpcomingMovieController(UpcomingMovieService upComingMovieService) {
+        this.upComingMovieService = upComingMovieService;
+    }
+
+    @GetMapping("/upcoming")
+    public ResponseEntity<List<UpcomingMovieDTO>> upcoming() {
+        return ResponseEntity.ok(upComingMovieService.findUpcomingMovieByDate());
+    }
+}
