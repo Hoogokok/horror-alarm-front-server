78cda8da2b8f363d3ed51bd38399b0c688a01486
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-15 17:37:39 +0900

    refactor: 개봉예정 영화 엔티티에 이미지 경로와 설명을 추가하고 제목, 개봉일, 이미지 경로에 null 값이 들어가지 않게 처리

--- src/main/java/org/alram/horroralarmbackend/releasemovie/UpComingMovie.java
+++ src/main/java/org/alram/horroralarmbackend/releasemovie/UpComingMovie.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Table(name = "upcoming_movie")
 @Entity
@@ -15,14 +16,21 @@ public class UpComingMovie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String releaseDate;
+    @NonNull
+    private String poster_path;
+    private String overview;
 
     public UpComingMovie() {
     }
 
-    public UpComingMovie(String title, String releaseDate) {
+    public UpComingMovie(String title, String releaseDate, String poster_path, String overview) {
         this.title = title;
         this.releaseDate = releaseDate;
+        this.poster_path = poster_path;
+        this.overview = overview;
     }
 }
