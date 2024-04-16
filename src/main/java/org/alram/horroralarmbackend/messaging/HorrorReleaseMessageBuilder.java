dfceb0416c2f10048c6ec9e8f6ab725e5be33fbd
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-16 13:09:47 +0900

    refactor: 메시지 작성 클래스의 파라미터를 DTO로 수정하고 메시지 내용을 수정함

--- src/main/java/org/alram/horroralarmbackend/messaging/HorrorReleaseMessageBuilder.java
+++ src/main/java/org/alram/horroralarmbackend/messaging/HorrorReleaseMessageBuilder.java
@@ -1,14 +1,20 @@
 package org.alram.horroralarmbackend.messaging;
 
 import java.util.List;
+import org.alram.horroralarmbackend.upcoming.UpcomingMovieDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HorrorReleaseMessageBuilder {
 
-    public MessageDTO buildMessage(String topic, List<String> releaseMovies) {
-        StringBuilder messageBuilder = new StringBuilder("오늘 개봉하는 공포영화 목록입니다.\n");
-        releaseMovies.forEach(releaseMovie -> messageBuilder.append(releaseMovie).append("\n"));
-        return new MessageDTO("오늘 개봉하는 공포영화 목록", messageBuilder.toString(),topic);
+    public MessageDTO buildMessage(String topic, List<UpcomingMovieDTO> upcomingMovies) {
+        StringBuilder messageBuilder = new StringBuilder("개봉하는 공포영화 목록입니다.\n");
+        upcomingMovies.forEach(upcomingMovie -> {
+            messageBuilder.append(upcomingMovie.title())
+                .append(" : ")
+                .append(upcomingMovie.releaseDate())
+                .append("\n");
+        });
+        return new MessageDTO("개봉하는 공포영화 목록", messageBuilder.toString(),topic);
     }
 }
