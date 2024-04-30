c0d03aea18970d017e500a222135fd18b390d60a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-04-30 21:19:39 +0900

    refactor: 임시로 만든 스트리밍 종료 예정일 타임라인 ui를 제목과 종료 예정일이 나오게 바꿈

--- src/main/horror-alram-front/src/streamingExpired.js
+++ src/main/horror-alram-front/src/streamingExpired.js
@@ -4,31 +4,36 @@ import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';;
+import TimelineDot from '@mui/lab/TimelineDot';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import Typography from '@mui/material/Typography';
 
 function StreamingExpired() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('/streaming/expired')
+    .then((response) => {
+      console.log(response);
+      setMovies(response.data.expiredMovies);
+    });
+  }, []);
+
   return (
       <Timeline position="alternate">
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Eat</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
+        {movies.map((movie) => (
+            <TimelineItem key={movie.id}>
+              <TimelineSeparator>
+                <TimelineDot color="primary"/>
+                <TimelineConnector/>
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography>{movie.title}</Typography>
+                <Typography>{movie.expiredDate}</Typography>
+              </TimelineContent>
+            </TimelineItem>
+        ))}
       </Timeline>
   );
 }
