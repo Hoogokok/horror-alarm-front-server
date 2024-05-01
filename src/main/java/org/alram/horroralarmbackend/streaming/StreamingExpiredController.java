3d06900b17fb8a6e9d0477dfed05b152d3b4a645
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-01 09:03:39 +0900

    refactor: id값을 받아오는 방식을 Pathvariable 방식으로 바꿈

--- src/main/java/org/alram/horroralarmbackend/streaming/StreamingExpiredController.java
+++ src/main/java/org/alram/horroralarmbackend/streaming/StreamingExpiredController.java
@@ -3,7 +3,7 @@ package org.alram.horroralarmbackend.streaming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +21,8 @@ public class StreamingExpiredController {
         return netflixExpiredService.getNetflixExpiredResponse();
     }
 
-    @GetMapping("/streaming/expired/detail")
-    public ExpiredDetailResponse getNetflixExpiredDetailResponse(@RequestParam("id") Long id) {
-        log.info("id: " + id);
+    @GetMapping("/streaming/expired/detail/{id}")
+    public ExpiredDetailResponse getNetflixExpiredDetailResponse(@PathVariable("id") Long id) {
         return netflixExpiredService.getNetflixExpiredDetailResponse(id);
     }
 }
