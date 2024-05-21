9fb91fffee04356a8631b8f733bd0148a7864d51
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-21 21:42:37 +0900

    refactor: 구독, 구독취소, 타임스탬프 업데이트 요청이 실패할 경우 400번 코드가 담긴  응답을 보냄

--- api/routes/firebase.ts
+++ api/routes/firebase.ts
@@ -27,127 +27,142 @@ app.use("/api/grantToken", grantToken);
 
 */
 async function subscribe(c: Context) {
+  const body = await c.req.json();
+  const { token, topic } = body;
+  try {
+    const result = await subscribed(token, topic);
+    if (!result.subscribe) {
+      return new Response(result.error, {
+        status: 400,
+      });
+    }
+    await messaging.subscribeToTopic(token, topic);
+    return new Response("구독 완료", {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
+  }
+}
+
+async function unsubscribe(c: Context) {
+  try {
     const body = await c.req.json();
     const { token, topic } = body;
-    try {
-      await subscribed(token, topic);
-      await messaging.subscribeToTopic(token, topic);
-      return new Response("구독 완료", {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
+    const result = await unsubscribed(token, topic);
+    if (!result.unsubscribe) {
+      return new Response(result.error, {
+        status: 400,
       });
     }
+    await messaging.unsubscribeFromTopic(token, topic);
+    return new Response("구독 취소", {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
   }
-  
-  async function unsubscribe(c: Context) {
-    try {
-      const body = await c.req.json();
-      const { token, topic } = body;
-      await unsubscribed(token, topic);
-      await messaging.unsubscribeFromTopic(token, topic);
-      return new Response("구독 취소", {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
+}
+
+async function permission(c: Context) {
+  try {
+    const body = await c.req.json();
+    const { token, time } = body;
+    const result = await grantToken(token, time);
+    if (!result.active) {
+      return new Response(result.error, {
+        status: 400,
       });
     }
+    return new Response(JSON.stringify(result.active), {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
   }
-  
-  async function permission(c: Context) {
-    try {
-      const body = await c.req.json();
-      const { token, time } = body;
-      const result = await grantToken(token, time);
-      if (!result.active) {
-        return new Response(result.error, {
-          status: 400,
-        });
-      }
-      return new Response(JSON.stringify(result.active), {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
+}
+
+async function timestamp(c: Context) {
+  try {
+    const body = await c.req.json();
+    const { oldToken, newToken, newTime } = body;
+    const result = await updateTokenTime(oldToken, newToken, newTime);
+    if (!result.updateResult) {
+      return new Response(result.error, {
+        status: 400,
       });
     }
+    return new Response(JSON.stringify(result), {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
   }
-  
-  async function timestamp(c: Context) {
-    try {
-      const body = await c.req.json();
-      const { oldToken, newToken, newTime } = body;
-      const result = await updateTokenTime(oldToken, newToken, newTime);
-      return new Response(JSON.stringify(result), {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
-      });
-    }
+}
+
+async function getTimestamp(c: Context) {
+  try {
+    const token = c.req.query("token");
+    const result = await checkTokenTimeStamps(token);
+    return new Response(JSON.stringify(result), {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
   }
-  
-  async function getTimestamp(c: Context) {
-    try {
-      const token = c.req.query("token");
-      const result = await checkTokenTimeStamps(token);
-      return new Response(JSON.stringify(result), {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
-      });
-    }
+}
+
+async function getSubscriptions(c: Context) {
+  try {
+    const token = c.req.query("token");
+    const result = await getTopics(token);
+    return new Response(JSON.stringify(result), {
+      headers: {
+        ...corsHeaders,
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
   }
-  
-  async function getSubscriptions(c: Context) {
-    try {
-      const token = c.req.query("token");
-      const result = await getTopics(token);
-      return new Response(JSON.stringify(result), {
-        headers: {
-          ...corsHeaders,
-          "content-type": "application/json",
-        },
-      });
-    } catch (error) {
-      return new Response(error, {
-        status: 500,
-      });
-    }
-  }
-  
-  function router(hono: Hono) {
-    const api = hono.basePath("/api");
-    api.post("/subscribe", (context) => subscribe(context));
-    api.post("/unsubscribe", (context) => unsubscribe(context));
-    api.post("/permission", (context) => permission(context));
-    api.post("/timestamp", (context) => timestamp(context));
-    api.get("/timestamp", (context) => getTimestamp(context));
-    api.get("/subscriptions", (context) => getSubscriptions(context));
-    return api;
-  }
-  
-  export { router };
+}
+
+function router(hono: Hono) {
+  const api = hono.basePath("/api");
+  api.post("/subscribe", (context) => subscribe(context));
+  api.post("/unsubscribe", (context) => unsubscribe(context));
+  api.post("/permission", (context) => permission(context));
+  api.post("/timestamp", (context) => timestamp(context));
+  api.get("/timestamp", (context) => getTimestamp(context));
+  api.get("/subscriptions", (context) => getSubscriptions(context));
+  return api;
+}
+
+export { router };
