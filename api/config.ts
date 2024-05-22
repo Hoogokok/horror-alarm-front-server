c3b8767869b1cbfb65a8eda0d55daff60084f924
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-22 09:29:51 +0900

    fix: 환경변수 설정 원래대로 고침

--- api/config.ts
+++ api/config.ts
@@ -1,9 +1,8 @@
-
 export const serviceAccount = {
     "type": Deno.env.get("REACT_APP_FIRE_BASE_TYPE"),
     "projectId": Deno.env.get("REACT_APP_FIRE_BASE_PROJECT_ID"),
     "privateKeyId": Deno.env.get("REACT_APP_FIRE_BASE_PRIVATE_KEY_ID"),
-    "privateKey": Deno.env.get("REACT_APP_FIRE_BASE_PRIVATE_KEY"),
+    "privateKey": Deno.env.get("REACT_APP_FIRE_BASE_PRIVATE_KEY")?.replace(/\\n/g, "\n"),
     "clientEmail": Deno.env.get("REACT_APP_FIRE_BASE_CLIENT_EMAIL"),
     "clientId": Deno.env.get("REACT_APP_FIRE_BASE_CLIENT_ID"),
     "authUri": Deno.env.get("REACT_APP_FIRE_BASE_AUTH_URI"),
