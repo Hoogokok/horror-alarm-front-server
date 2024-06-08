6beea9e90e7117a6c4cc91022eac0b2889fe3fc7
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-09 00:16:25 +0900

    refactor: cors origin 값을 env 환경 변수 설정에 읽어옴ㅁ

--- api/_shared/cors.ts
+++ api/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": Deno.env.get("ALLOWED_ORIGIN") as string,
     "Access-Control-Allow-Headers":
       "authorization, x-client-info, apikey, content-type",
   };
