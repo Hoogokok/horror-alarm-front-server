6a428cd1029a46a737b664867da7fea382672c93
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-09 00:15:04 +0900

    feat: cores 설정 추가

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-import { cors } from 'https://deno.land/x/hono/middleware.ts'
+import { cors } from "https://deno.land/x/hono/middleware.ts";
 import { router } from "./api/routes/firebase.ts";
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+// import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+// const env = await load();
 
 const app = new Hono();
 
-app.use('/api/*', cors())
+app.use("/api/*",
+    cors({
+        origin: Deno.env.get("ALLOWED_ORIGIN") as string,
+        allowHeaders: ['X-Custom-Header', 'authorization', 'x-client-info', 'apikey', 'content-type'],
+        allowMethods: ['GET', 'POST', 'DELETE'],
+        maxAge: 600,
+    })
+);
 router(app);
 
-Deno.serve(app.fetch)
+Deno.serve(app.fetch);
